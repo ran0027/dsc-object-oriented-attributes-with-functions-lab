760119d50d17058be10d393b9db395de45be7db1
Rebecca Neel <rebecc.clark@gmail.com>
2022-11-19 14:12:29 -0800

    Complete lab

--- school.py
+++ school.py
@@ -1,1 +1,23 @@
 class School:
+
+    def __init__(self, name, roster={}):
+        self.name = name
+        self.roster = roster
+        
+    def add_student(self, name, grade_level):
+        if grade_level in self.roster.keys():
+            self.roster[grade_level].append(name)
+        else:
+            self.roster.update({grade_level: [name]})
+            
+    def grade(self, grade):
+        return self.roster[grade]
+    
+    def sort_roster(self):
+        sorted_roster = {}
+        for k, v in self.roster.items():
+            sorted_roster[k] = sorted(v)
+        return sorted_roster
+    
+    def __repr__(self):
+        return f'Class: School, {self.name}'
